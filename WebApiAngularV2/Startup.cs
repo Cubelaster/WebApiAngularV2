6253dfbffbd326372a1dc86cd11beca3042a83ae
Cubelaster <david.cubela@gmail.com>
2017-08-08 13:07:53 +0200

    missed a file

--- WebApiAngularV2/Startup.cs
+++ WebApiAngularV2/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Configuration;
