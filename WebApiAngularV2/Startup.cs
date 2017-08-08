a1a911ef8832bd29dc13ce0a0a79ac6b80b884d1
Cubelaster <david.cubela@gmail.com>
2017-08-08 14:05:19 +0200

    Figured what was wrong
    OK, so the problem was that the solution wasn't serving static files and that's why i needed to do a ng serve and run the app at the same time.
    I had to actually manually create wwwroot folder (ignored in git...) and then ng build for the solution to get the wwwroot folder. Also, VS got stupid and needed to get restarted. After that, the server served the static files from wwwroot like angular was working.
    Also made some small changes to startup and launch settings.

--- WebApiAngularV2/Startup.cs
+++ WebApiAngularV2/Startup.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Server.Features;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System.IO;
 
 namespace WebApiAngularV2
 {
   public class Startup
   {
-    public Startup(IHostingEnvironment env)
+    public Startup(IHostingEnvironment env, ILogger<Startup> _logger)
     {
       var builder = new ConfigurationBuilder()
           .SetBasePath(env.ContentRootPath)
           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
           .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
           .AddEnvironmentVariables();
+
+      this.logger = _logger;
       Configuration = builder.Build();
     }
 
     public IConfigurationRoot Configuration { get; }
+    private ILogger<Startup> logger { get; set; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
@@ -38,36 +37,41 @@ namespace WebApiAngularV2
       loggerFactory.AddDebug();
 
       #region OnServerRun
-      var serverAddressesFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
-      app.Run(async (context) =>
-      {
-        context.Response.ContentType = "text/html";
-        await context.Response.WriteAsync("<p>Hosted by Kestrel</p>");
+      // This writes instead of response. Proves server is alive.
+      // It also overwrites the response. :)
 
-        if (serverAddressesFeature != null)
-        {
-          await context.Response.WriteAsync("<p>Listening on the following addresses: " +
-            string.Join(", ", serverAddressesFeature.Addresses) + "</p>");
-        }
+      //var serverAddressesFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
+      //app.Run(async (context) =>
+      //{
+      //  context.Response.ContentType = "text/html";
+      //  await context.Response.WriteAsync("<p>Hosted by Kestrel</p>");
 
-        await context.Response.WriteAsync($"<p>Request URL: {context.Request.GetDisplayUrl()}<p>");
-      });
+      //  if (serverAddressesFeature != null)
+      //  {
+      //    await context.Response.WriteAsync("<p>Listening on the following addresses: " +
+      //      string.Join(", ", serverAddressesFeature.Addresses) + "</p>");
+      //  }
+
+      //  await context.Response.WriteAsync($"<p>Request URL: {context.Request.GetDisplayUrl()}<p>");
+      //});
       #endregion OnServerRun
 
       app.Use(async (context, next) =>
       {
         await next();
-        if (context.Response.StatusCode == 404 &&
-           !Path.HasExtension(context.Request.Path.Value) &&
-           !context.Request.Path.Value.StartsWith("/api/"))
+        if (context.Response.StatusCode == 404 
+            //&& !Path.HasExtension(context.Request.Path.Value)
+            //&& !context.Request.Path.Value.StartsWith("/api/")
+           )
         {
+          logger.LogInformation("Redirected from 404 to index");
           context.Request.Path = "/index.html";
           await next();
         }
       });
 
-      app.UseDefaultFiles();
       app.UseStaticFiles();
+      app.UseDefaultFiles();
 
       app.UseMvc();
       // Configures application for usage as API
