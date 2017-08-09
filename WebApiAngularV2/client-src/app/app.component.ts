38ab4a98f30085dcfb9afcbd1600b464e7be010a
Cubelaster <david.cubela@gmail.com>
2017-08-09 09:55:43 +0200

    Started making changes to angular part
    Remember to CLEAR THE CACHE OF BROWSER!!!!!!

--- WebApiAngularV2/client-src/app/app.component.ts
+++ WebApiAngularV2/client-src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent implements OnInit {
     constructor(private _httpService: Http) { }
 
     apiValues: string[] = [];
+    title = "Tour of Heroes";
+    hero = 'Windstorm';
 
     ngOnInit() {
         this._httpService.get('/api/values').subscribe(values => {
