004c65b2e569a5593a5d4987fe74f9b0a10a57c4
Cubelaster <david.cubela@gmail.com>
2017-10-18 18:32:21 +0200

    Missed part

--- BL/Controllers/AuthController.cs
+++ BL/Controllers/AuthController.cs
@@ -3,10 +3,13 @@ using BL.Security.SecurityContracts;
 using BL.ViewModels.Account;
 using DAL.Models.IdentityClasses;
 using DAL.Models.Security;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -17,13 +20,18 @@ namespace BL.Controllers
     public class AuthController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IJwtFactory _jwtFactory;
         private readonly JsonSerializerSettings _serializerSettings;
         private readonly JwtIssuerOptions _jwtOptions;
 
-        public AuthController(UserManager<ApplicationUser> userManager, IJwtFactory jwtFactory, IOptions<JwtIssuerOptions> jwtOptions)
+        public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,
+            RoleManager<IdentityRole> roleManager, IJwtFactory jwtFactory, IOptions<JwtIssuerOptions> jwtOptions)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
             _jwtFactory = jwtFactory;
             _jwtOptions = jwtOptions.Value;
 
@@ -42,44 +50,59 @@ namespace BL.Controllers
                 return BadRequest(ModelState);
             }
 
-            var identity = await GetClaimsIdentity(credentials.UserName, credentials.Password);
-            if (identity == null)
+            var result = await _signInManager.PasswordSignInAsync(credentials.UserName, credentials.Password, true, true);
+            if (result.Succeeded)
             {
-                return BadRequest(Errors.AddErrorToModelState("login_failure", "Invalid username or password.", ModelState));
+                var user = await _userManager.FindByNameAsync(credentials.UserName);
+                var userClaims = await GetUserClaims(user);
+
+                // Serialize and return the response
+                var response = new
+                {
+                    id = userClaims.Single(c => c.Type == "id").Value,
+                    auth_token = await _jwtFactory.GenerateEncodedToken(userClaims),
+                    expires_in = (int)_jwtOptions.ValidFor.TotalSeconds
+                };
+
+                var json = JsonConvert.SerializeObject(response, _serializerSettings);
+                return new OkObjectResult(json);
             }
 
-            // Serialize and return the response
-            var response = new
-            {
-                id = identity.Claims.Single(c => c.Type == "id").Value,
-                auth_token = await _jwtFactory.GenerateEncodedToken(credentials.UserName, identity),
-                expires_in = (int)_jwtOptions.ValidFor.TotalSeconds
-            };
-
-            var json = JsonConvert.SerializeObject(response, _serializerSettings);
-            return new OkObjectResult(json);
+            return BadRequest(Errors.AddErrorToModelState("login_failure", "Invalid username or password.", ModelState));
         }
 
-        private async Task<ClaimsIdentity> GetClaimsIdentity(string userName, string password)
+        private async Task<List<Claim>> GetUserClaims(ApplicationUser user)
         {
-            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(password))
-            {
-                // get the user to verifty
-                var userToVerify = await _userManager.FindByNameAsync(userName);
-                var roles = await _userManager.GetRolesAsync(userToVerify);
+            IdentityOptions identityOptions = new IdentityOptions();
 
-                if (userToVerify != null)
+            // get default JWT claims
+            var claims = await _jwtFactory.GetJWTClaims(user);
+            // Add Identity claims
+            claims.AddRange(new List<Claim>() {
+                new Claim(identityOptions.ClaimsIdentity.UserIdClaimType, user.Id.ToString()),
+                new Claim(identityOptions.ClaimsIdentity.UserNameClaimType, user.UserName)
+            });
+
+            // Get additional claims linked to user, without role and get roles as well
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+            claims.AddRange(userClaims);
+
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, userRole));
+                var role = await _roleManager.FindByNameAsync(userRole);
+                if (role != null)
                 {
-                    // check the credentials  
-                    if (await _userManager.CheckPasswordAsync(userToVerify, password))
+                    var roleClaims = await _roleManager.GetClaimsAsync(role);
+                    foreach (Claim roleClaim in roleClaims)
                     {
-                        return await Task.FromResult(_jwtFactory.GenerateClaimsIdentity(userName, userToVerify.Id));
+                        claims.Add(roleClaim);
                     }
                 }
             }
 
-            // Credentials are invalid, or account doesn't exist
-            return await Task.FromResult<ClaimsIdentity>(null);
+            return await Task.FromResult(claims);
         }
     }
 }
